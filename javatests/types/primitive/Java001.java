921085ba1b52eccc929554d5ae3e068a1716d50f
serialversionuid <serialversionuid@gmail.com>
2016-05-18 21:42:54 +0200

    Added Java001, updated gitignore

---
+++ javatests/types/primitive/Java001.java
@@ -0,0 +1,28 @@
+package types.primitive;
+
+/**
+ * @author serialversionuid
+ */
+public class Java001 {
+
+    /*
+
+    What will be the result of the following code execution?
+
+    false
+
+    true
+
+    Result: false
+
+    Result: true
+
+     */
+
+    public static void main(String[] args) {
+        String s1 = "str";
+        String s2 = "str";
+        System.out.println("Result: " + s1 == s2);
+    }
+
+}
