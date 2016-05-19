71156618c3172bbd38249123f340680a3307c009
maptoint <maptoint@gmail.com>
2016-05-19 15:05:20 +0200

    Added Java002

---
+++ javatests/types/primitive/Java002.java
@@ -0,0 +1,37 @@
+package types.primitive;
+
+/**
+ * @author maptoint
+ */
+public class Java002 {
+
+    /*
+
+    	What is the result? 
+	
+	A. 123456
+	
+	B. An exception could be thrown at runtime.
+
+	C. 654321
+	
+	D. nothing
+
+	E. 65432
+
+	F. 12345
+
+
+     */
+
+    public static void main(String[] args) {
+        int []a = {1,2,3,4,5,6};
+        int i = a.length - 1;
+    
+        while(i>=0){
+        System.out.print(a[i]);
+        i--;
+    } 
+    }
+
+}
