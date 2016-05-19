23afe51e6462792c0ac1bf3f5644fa252df79fa5
maptoint <maptoint@gmail.com>
2016-05-19 16:28:06 +0200

    Modific Java003

--- javatests/types/primitive/Java003.java
+++ javatests/types/primitive/Java003.java
@@ -1,4 +1,21 @@
 class Java003 {
+
+	/*
+    
+        What is the result?
+
+        A. 0
+
+        B. error
+
+        C. Compilation fails
+
+        D. An uncaught exception is thrown at runtime.
+
+        E. No output
+
+    */
+
     public static void main(String args[]) {
         int x = 0, y = 10;
         try {
