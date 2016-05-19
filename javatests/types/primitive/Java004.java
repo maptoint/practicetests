ae3f126f5e75065479f3f55f9c804cce6a55dd41
serialversionuid <serialversionuid@gmail.com>
2016-05-19 23:12:22 +0200

    Added Java004

---
+++ javatests/types/primitive/Java004.java
@@ -0,0 +1,28 @@
+package types.primitive;
+
+    /*
+
+    What will be the result of the following code execution?
+
+    Compilation error
+
+    11-1
+
+    22-2
+
+    12-2
+
+     */
+
+public class Java004 {
+
+    public static void main(String[] args) {
+//        byte a = 1;
+//        byte b = ++a;
+//        byte c = -a;
+//        System.out.print(a);
+//        System.out.print(b);
+//        System.out.print(c);
+    }
+
+}
