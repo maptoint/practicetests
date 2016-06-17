d63d2eda1c63ae8611360cbd29a4917435ed0519
maptoint <maptoint@gmail.com>
2016-06-17 22:59:37 +0200

    Add from Java038 to Java050

---
+++ javatests/types/primitive/Java038.java
@@ -0,0 +1,7 @@
+package types.primitive;
+
+/**
+ * Created by maptoint on 6/11/16.
+ */
+public class Java038 {
+}
---
+++ javatests/types/primitive/Java049.java
@@ -0,0 +1,7 @@
+package types.primitive;
+
+/**
+ * Created by maptoint on 6/17/16.
+ */
+public class Java049 {
+}
